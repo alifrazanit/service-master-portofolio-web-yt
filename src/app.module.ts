65e391a8f3fe1086c78e16712b1359882bbee77b
Alif Razan Saputra <alifrazanit@gmail.com>
2025-01-07 23:11:37 +0700

    setup config db app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,21 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserController } from './controllers/master/user/user.controller';
 import { UserService } from './services/user/user.service';
- // tes
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host:'localhost',
+      port: 5432,
+      username: 'portofolio_website_stag',
+      password: '123456',
+      database: 'portofolio_website_stag',
+      autoLoadEntities: true,
+      synchronize: true
+    })
+  ],
   controllers: [AppController, UserController],
   providers: [AppService, UserService],
 })
